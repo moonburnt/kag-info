1600f169295b670ff5a1b7c92a33367a6fd60713
moonburnt <moonburnt@disroot.org>
2020-08-09 01:38:16 +0300

    Add files via upload

--- kag-info.py
+++ kag-info.py
@@ -3,8 +3,10 @@
 ##Imports
 import requests #to get json with data from kag's api
 import json #to turn json into python's dictionary
-import re #to remove MODS USED part from description of modded servers
 import argparse #for launch arguments used to do various shenanigans with output
+from re import sub as replace #to fix server's description and filter ping's output
+from platform import system as os_name #to apply os-specific flag in server_ms
+from subprocess import check_output #to get output of system's ping command in server_ms
 
 kagprefix = "kag://"
 
@@ -31,24 +33,27 @@ def player_amount(servers):
         players += len(x['playerList'])
     return players
 
-def server_info(server):
-    '''Expects to receive dictionary featuring server's info. Prints interesting entries out of it'''
-    name = server['name']
-    raw_description = server['description'] #this one is kinda messy, coz it also contains info about used mods, and its all over the place. Blame kag api for that
-    description = re.sub("\n\n.*", "", raw_description) #because of reason above, Im cleaning it up. MODS USED usually go after two empty lines, so I delet them and everything after
-    ip = server['IPv4Address']
-    port = server['port']
-    password_protected = server['password']
-    kaglink = kagprefix+str(ip)+":"+str(port)
-    gamemode = server['gameMode']
-    using_mods = server['usingMods']
-    players = len(server['playerList'])
-    maxplayers = server['maxPlayers']
-    player_amount = str(players)+"/"+str(maxplayers)
-    spectators = server['spectatorPlayers']
-    nicknames = ', '.join(server['playerList'])
+def server_country(ip):
+    '''Receives server IP (str), returns server's country (str) based on geojs.io data'''
+    link = "https://get.geojs.io/v1/ip/country/"+ip+".json"
+    response = requests.get(link, timeout = 30)
+    pydic = json.loads(response.text)
+    country = pydic["name"]
+    return country
 
-    print("\nName: {}\nDescription: {}\nAddress: {}\nRequires Password: {}\nGamemode: {}\nUsing Mods: {}\nPlayers: {} \nSpectators: {} \nCurrently Playing: {}".format(name, description, kaglink, password_protected, gamemode, using_mods, player_amount, spectators, nicknames))
+def server_ms(address):
+    '''Receives server IP (str), returns ms to that server (str)'''
+    #You have no idea how much salt did that crappy function produce
+    if os_name() == 'Windows':
+        flag = '-n'
+    else:
+        flag = '-c'
+
+    command = "ping", flag, "1", address
+    raw_answer = check_output(command) #calling system's ping command and getting output into variable
+    x = replace('^.*time=', "", str(raw_answer)) #cutting output's intro before 'time='
+    answer = replace(' ms.*', "", str(x)) #cutting everything after response time's int
+    return answer
 
 def players_amount(x):
     '''Receives dictionary entry with server's info. Returns amount of players on server'''
@@ -60,11 +65,19 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("-ni", "--nointro", help="Doesnt show info message about connecting to api", action="store_true")
     argparser.add_argument("-nd", "--nodetails", help="Only shows global online statistics, without detailed info about active servers", action="store_true")
-    argparser.add_argument("-sm", "--skip_modded", help="Shows only vanilla (non-modded) servers", action="store_true")
-    argparser.add_argument("-sv", "--skip_vanilla", help="Shows only modded servers", action="store_true")
-    argparser.add_argument("-spr", "--skip_private", help="Shows only public servers", action="store_true")
-    argparser.add_argument("-spu", "--skip_public", help="Shows only password-protected servers", action="store_true")
-    argparser.add_argument("-sf", "--skip_full", help="Shows only servers that have free space", action="store_true")
+    argparser.add_argument("-sm", "--skip_modded", help="Show only vanilla (non-modded) servers", action="store_true")
+    argparser.add_argument("-sv", "--skip_vanilla", help="Show only modded servers", action="store_true")
+    argparser.add_argument("-spr", "--skip_private", help="Show only public servers", action="store_true")
+    argparser.add_argument("-spu", "--skip_public", help="Show only password-protected servers", action="store_true")
+    argparser.add_argument("-sf", "--skip_full", help="Show only servers that have free space", action="store_true")
+    argparser.add_argument("-hc", "--hide_country", help="Hide server's country", action="store_true")
+    argparser.add_argument("-hn", "--hide_names", help="Hide list of players playing on server", action="store_true")
+    argparser.add_argument("-hs", "--hide_spectators", help="Hide amount of spectators on server", action="store_true")
+    argparser.add_argument("-hg", "--hide_gamemode", help="Hide server's game mode", action="store_true")
+    argparser.add_argument("-hd", "--hide_description", help="Hide server's description", action="store_true")
+    argparser.add_argument("-hp", "--hide_protection", help="Hide state of server's password protection", action="store_true")
+    argparser.add_argument("-hm", "--hide_mods", help="Hide state of server's mods usage", action="store_true")
+    argparser.add_argument("-hpi", "--hide_ping", help="Hide ping to server", action="store_true")
     argparser.parse_args()
 
     args = argparser.parse_args()
@@ -84,15 +97,54 @@ if __name__ == "__main__":
 
     if not args.nodetails:
         print("-------------\nServers info:")
-        for x in active_servers:
-            if args.skip_modded and x['usingMods']:
+        for server in active_servers:
+            name = server['name']
+            raw_description = server['description'] #this one is kinda messy, coz it also contains info about used mods, and its all over the place. Blame kag api for that
+            description = replace("\n\n.*", "", raw_description) #because of reason above, Im cleaning it up. MODS USED usually go after two empty lines, so I delet them and everything after
+            ip = server['IPv4Address']
+            if not args.hide_country: #I made it this way, because its request to third party server that may consume some time
+                country = server_country(ip)
+            if not args.hide_ping: #same as above
+                ping = server_ms(ip)
+            port = server['port']
+            password_protected = server['password']
+            kaglink = kagprefix+str(ip)+":"+str(port)
+            gamemode = server['gameMode']
+            using_mods = server['usingMods']
+            players = len(server['playerList'])
+            maxplayers = server['maxPlayers']
+            player_amount = str(players)+"/"+str(maxplayers)
+            spectators = server['spectatorPlayers']
+            nicknames = ', '.join(server['playerList'])
+
+            if args.skip_modded and using_mods:
                 continue
-            if args.skip_vanilla and not x['usingMods']:
+            if args.skip_vanilla and not using_mods:
                 continue
-            if args.skip_private and x['password']:
+            if args.skip_private and password_protected:
                 continue
-            if args.skip_public and not x['password']:
+            if args.skip_public and not password_protected:
                 continue
-            if args.skip_full and (len(x['playerList']) >= int(x['maxPlayers'])):
+            if args.skip_full and (players >= maxplayers):
                 continue
-            server_info(x)
+
+            #Dis ugly as hell coz I found no other way to apply flags
+            print("\nName: {}".format(name))
+            if not args.hide_description:
+                print("Description: {}".format(description))
+            if not args.hide_country:
+                print("Country: {}".format(country))
+            print("Address: {}".format(kaglink))
+            if not args.hide_ping:
+                print("Ping: {} ms".format(ping))
+            if not args.hide_protection:
+                print("Requires Password: {}".format(password_protected))
+            if not args.hide_gamemode:
+                print("Gamemode: {}".format(gamemode))
+            if not args.hide_mods:
+                print("Using Mods: {}".format(using_mods))
+            print("Players: {}".format(player_amount))
+            if not args.hide_spectators:
+                print("Spectators: {}".format(spectators))
+            if not args.hide_names:
+                print("Currently Playing: {}".format(nicknames))
